ba6103b71f8470f129b4b0b89beb8f34232e6166
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-12-17 14:24:36 +0530

    Creating initial extension

---
+++ extension.js
@@ -0,0 +1,38 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "simple-imports" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+        // The code you place here will be executed every time your command is executed
+
+        // Get current text editor
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('Please open a editor window!');
+            return; // No open text editor
+        } else if (editor.document.isUntitled) {
+            vscode.window.showErrorMessage('Please save the current file first!');
+            return;
+        }
+        vscode.window.showInformationMessage('Destination: ' + editor.document.fileName);
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
