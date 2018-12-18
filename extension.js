00389156ff2c40c99921fcf842eeb2be71d956b4
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-12-18 19:56:17 +0530

    getCurrentFile: Adding function for getting current open file

--- extension.js
+++ extension.js
@@ -2,30 +2,44 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+// this method returns the path of the current file open
+// if it is saved. Else, returns error
+
+const getCurrentFile = () => { 
+    // Get current text editor
+    let editor = vscode.window.activeTextEditor;
+
+    if(!editor) {
+        return {
+            error: true,
+            errorMessage: 'Open a file first!'
+        };
+    } else if (editor.document.isUntitled) {
+        return {
+            error: true,
+            errorMessage: 'Save the destination file first'
+        };
+    }
+    return {
+        filename: editor.document.fileName
+    };
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "simple-imports" is now active!');
-
+const activate = (context) => {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.simpleImport', function () {
         // The code you place here will be executed every time your command is executed
+        const {filename, error, errorMessage} = getCurrentFile();
 
-        // Get current text editor
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            vscode.window.showErrorMessage('Please open a editor window!');
-            return; // No open text editor
-        } else if (editor.document.isUntitled) {
-            vscode.window.showErrorMessage('Please save the current file first!');
+        if(error) {
+            vscode.window.showErrorMessage(errorMessage);
             return;
         }
-        vscode.window.showInformationMessage('Destination: ' + editor.document.fileName);
+        vscode.window.showInformationMessage('Destination: ' + filename);
     });
 
     context.subscriptions.push(disposable);
